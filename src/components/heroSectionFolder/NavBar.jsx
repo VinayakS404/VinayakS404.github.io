14776c1908b30de5a881ef592b806d2857a11b30
Vinayak S <oreva.vinayak@gmail.com>
2026-03-20 23:39:22 +0530

    feat: add active color in nav btns

--- src/components/heroSectionFolder/NavBar.jsx
+++ src/components/heroSectionFolder/NavBar.jsx
@@ -1,28 +1,48 @@
 import hamburgerIcon from "../../assets/hamburger.png";
+
+import { useState } from "react";
 //import chess from "../assets/knight.png";
 function NavBar({ setIsExtend, scrollRefs }) {
+  const [active, setActive] = useState("");
   const scrollTo = (id) => {
     const element = scrollRefs.current[id];
     if (element) {
       element.scrollIntoView({ behavior: "smooth" });
-      setIsExtend(false); // Close mobile menu if it's open
+      setIsExtend(false);
     }
   };
   function extendNavBar() {
-    setIsExtend(prev => !prev);
+    setIsExtend((prev) => !prev);
   }
   const navBarTittle = ["Home", "Skill", "About", "Project", "Contact"];
   const navArray = navBarTittle.map((item) => {
     return (
-      <li className="relative inline-flex items-center  overflow-hidden  rounded  group py-1.5 px-8" key={item} onClick={() => scrollTo(item.toLowerCase())}>
-        <span className="w-56 h-48 rounded bg-[#404047] absolute bottom-0 left-0 translate-x-full ease-in-out duration-300 transition-all translate-y-full mb-9 ml-9 group-hover:ml-0 group-hover:mb-32 group-hover:translate-x-0"></span>
-        <span className="relative w-full  text-white transition-colors duration-300 ease-in-out group-hover:text-white">
+      <li
+        key={item}
+        onClick={() => {
+          setActive(item);
+          scrollTo(item.toLowerCase());
+        }}
+        className={`
+          relative inline-flex items-center overflow-hidden rounded group py-1.5 px-8 transition-all
+          ${active === item ? "bg-blue-500 " : "text-white"}
+        `}
+      >
+        <span
+          className={`
+            w-56 h-48 rounded bg-[#404047] absolute bottom-0 left-0 translate-x-full ease-in-out duration-300 transition-all translate-y-full mb-9 ml-9 
+            group-hover:ml-0 group-hover:mb-32 group-hover:translate-x-0 
+            ${active === item ? "hidden" : "block"}
+          `}
+        ></span>
+
+        <span className="relative w-full transition-colors duration-300 ease-in-out">
           {item}
         </span>
       </li>
     );
   });
-  document
+  document;
   return (
     <div className="fixed z-50 top-0 left-0 ">
       <nav className="hidden md:flex fixed z-50 h-15 w-full px-20 bg-[#202021]/60 backdrop-blur-xl items-center justify-end text-xl ]  border-b border-[#313136] ">
