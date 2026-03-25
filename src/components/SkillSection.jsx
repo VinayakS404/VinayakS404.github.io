7ccf863e467311332c005d895474a1cb02d74f62
Vinayak S <oreva.vinayak@gmail.com>
2026-03-25 23:39:42 +0530

    feat: update SkillSection layout and styling for improved presentation

--- src/components/SkillSection.jsx
+++ src/components/SkillSection.jsx
@@ -44,42 +44,46 @@ function SkillSection() {
   });
 
   return (
-    <div className="relative  mt-50 flex flex-col items-center px-10 ">
+    <div className="relative mt-50 flex flex-col  px-10 ">
       <div className={`${bgBlurOverlay} ${sectionBase} absolute`} />
 
-      <div className="relative z-10 w-full flex flex-col items-center md:px-10 ">
-        <div className="flex flex-col items-start">
-          <p className="text-white font-['Anton'] text-[45px] md:text-6xl w-full max-w-2xl leading-tight">
-            Current&nbsp;
-            <span className="text-blue-500 font-bold block md:inline">
-              technologies
-            </span>
-          </p>
-          <p className="text-gray-300 max-w-6xl pt-4 md:pt-8 text-base md:text-[19px]">
-            These are the technologies I'm currently working with as I continue
-            building projects and improving my
-            <span className="text-blue-500 font-bold">
-              &nbsp;practical skills&nbsp;
-            </span>
-          </p>
-        </div>
-
+      <div className="relative z-10 w-full flex flex-col items-center justify-center md:px-10 ">
         <div>
-          <p className="text-white mt-20 font-['Anton'] text-4xl">Front-end</p>
-          <p className="text-gray-300 pt-5 max-w-3xl text-base md:text-[19px]">
-            The part of a website users see and interact with, including layout,
-            design, and basic interactivity
-          </p>
-          <div className={`${cardGridClass}`}>{frontEndTech}</div>
-        </div>
+          <div className="flex flex-col items-start ">
+            <p className="text-white font-['Anton'] text-[45px] md:text-6xl w-full max-w-2xl leading-tight">
+              Current&nbsp;
+              <span className="text-blue-500 font-bold block md:inline">
+                technologies
+              </span>
+            </p>
+            <p className="text-gray-300 max-w-6xl pt-4 md:pt-8 text-base md:text-[19px]">
+              These are the technologies I'm currently working with as I
+              continue building projects and improving my
+              <span className="text-blue-500 font-bold">
+                &nbsp;practical skills&nbsp;
+              </span>
+            </p>
+          </div>
 
-        <div>
-          <p className="text-white mt-20 font-['Anton'] text-4xl">Back-end</p>
-          <p className="text-gray-300 mt-5 max-w-3xl text-base md:text-[19px]">
-            The part of a website that works behind the scenes, handling server
-            logic, databases, and data processing
-          </p>
-          <div className={`${cardGridClass}`}>{backEndTech}</div>
+          <div className="flex flex-col items-start">
+            <p className="text-white mt-20 font-['Anton'] text-4xl">
+              Front-end
+            </p>
+            <p className="text-gray-300 pt-5 max-w-3xl text-base md:text-[19px]">
+              The part of a website users see and interact with, including
+              layout, design, and basic interactivity
+            </p>
+            <div className={`${cardGridClass}`}>{frontEndTech}</div>
+          </div>
+
+          <div className="flex flex-col items-start">
+            <p className="text-white mt-20 font-['Anton'] text-4xl">Back-end</p>
+            <p className="text-gray-300 mt-5 max-w-3xl text-base md:text-[19px]">
+              The part of a website that works behind the scenes, handling
+              server logic, databases, and data processing
+            </p>
+            <div className={`${cardGridClass}`}>{backEndTech}</div>
+          </div>
         </div>
       </div>
     </div>
