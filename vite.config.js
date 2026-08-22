ce02f3073e285ec178335508a7400503a6403532
Vinayak S <oreva.vinayak@gmail.com>
2026-08-22 11:59:36 +0530

    update

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,17 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
-import { qrcode } from "vite-plugin-qrcode"; // 1. Import it
+import { qrcode } from "vite-plugin-qrcode";
 
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
-    qrcode(), // 2. Add it here
+    qrcode(),
   ],
-  base: "/",
+
+  base: "/vinayak.github.io/",
+
   server: {
     host: true,
     port: 5173,
