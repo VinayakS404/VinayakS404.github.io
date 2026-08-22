1dd40a5742f143552be6168b6e9d0dd8eb776576
Vinayak S <oreva.vinayak@gmail.com>
2026-08-22 12:12:04 +0530

    Remove unnecessary router

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import { Routes, Route } from "react-router";
 import { useState, useRef } from "react";
 import "./App.css";
 
@@ -15,38 +14,31 @@ function App() {
   const scrollRefs = useRef({});
 
   return (
-    <Routes>
-      <Route
-        path="/"
-        element={
-          <>
-            <section ref={(el) => (scrollRefs.current.home = el)}>
-              <HeroSection
-                isExtend={isExtend}
-                setIsExtend={setIsExtend}
-                scrollRefs={scrollRefs}
-              />
-            </section>
+    <>
+      <section ref={(el) => (scrollRefs.current.home = el)}>
+        <HeroSection
+          isExtend={isExtend}
+          setIsExtend={setIsExtend}
+          scrollRefs={scrollRefs}
+        />
+      </section>
 
-            <section ref={(el) => (scrollRefs.current.skill = el)}>
-              <SkillSection />
-            </section>
+      <section ref={(el) => (scrollRefs.current.skill = el)}>
+        <SkillSection />
+      </section>
 
-            <section ref={(el) => (scrollRefs.current.about = el)}>
-              <AboutMe />
-            </section>
+      <section ref={(el) => (scrollRefs.current.about = el)}>
+        <AboutMe />
+      </section>
 
-            <section ref={(el) => (scrollRefs.current.project = el)}>
-              <ProjectSection />
-            </section>
+      <section ref={(el) => (scrollRefs.current.project = el)}>
+        <ProjectSection />
+      </section>
 
-            <section ref={(el) => (scrollRefs.current.contact = el)}>
-              <Contact />
-            </section>
-          </>
-        }
-      />
-    </Routes>
+      <section ref={(el) => (scrollRefs.current.contact = el)}>
+        <Contact />
+      </section>
+    </>
   );
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,10 @@
 import { StrictMode } from "react";
-import { BrowserRouter } from "react-router";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter basename="/vinayak.github.io">
-      <App />
-    </BrowserRouter>
+    <App />
   </StrictMode>,
 );
